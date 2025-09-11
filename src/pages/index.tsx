f64c289dcc28864ff9dad4b0e07644a0127fe74c
li-daqian <lidaqian11@gmail.com>
2025-09-11 13:23:36 +0800

    fix: update SpeedInsights import from '@vercel/speed-insights/next' to '@vercel/speed-insights/react'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
 import Hero from '@site/src/components/landing/Hero'
 import Particles from '@site/src/components/magicui/particles'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext()
@@ -13,6 +13,7 @@ export default function Home(): ReactNode {
         <Hero />
         <Particles className="absolute inset-0" quantity={100} ease={80} color="#ffffff" refresh />
       </main>
+      <SpeedInsights />
     </Layout>
   )
 }
